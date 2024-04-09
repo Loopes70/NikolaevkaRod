4349e9ba74fa6003a603f82a9072f0c9ad200fe5
LoopesWork <i.sheh@mail.ru>
2024-04-09 22:18:15 +0700

    Почистил от старого и ненужного

--- front/src/Header.js
+++ front/src/Header.js
@@ -1,8 +1,13 @@
+import imgHeader from './img/FamilyTree_30181.png';
+
 export default function Header () {
 
   return (
     <div className="header">
-      Как же здорово залить React-проект на GitHub
+      <div className="header-img">
+        <img src={imgHeader} alt="tree" width="75" height="75"/>
+      </div>
+      <div className="header-text">Род — группа людей, возводящих своё происхождение к общему предку-родоначальнику по одной родословной линии</div>
     </div>
   );
 
--- front/src/api.js
+++ front/src/api.js
@@ -2,35 +2,11 @@
 
 // 
 
-export async function getTime() {
+export default async function getTime() {
   return await api('get_time', {
     undefined
   });
 }
-
-export async function addTask(text) {
-  return await api('add_task', {
-    text: text,
-  });
-}
-
-export async function removeTask(id) {
-  return await api('remove_task', {
-    id,
-  });
-}
-
-export async function changeTaskStatus(id, completed) {
-  return await api('change_task_status', {
-    id,
-    completed,
-  });
-}
-
-export default async function getTasks() {
-  return await api('get_tasks', undefined);
-}
-
 
 async function api(action, body) {
   const options = {
